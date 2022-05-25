5805f49685d3e1facb01a2375a6395c4c2a15b65
Firoz Ahmed <firofame@gmail.com>
2022-05-25 16:59:02 +0530

    ReactPlayer

---
+++ src/App/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactPlayer from 'react-player'
+
+export default function App() {
+  return (
+    <div>
+        <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
+    </div>
+  )
+}
