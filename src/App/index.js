bb1766ea26ba3f40ba67c130f55a2244fa1e9bd6
Firoz Ahmed <firofame@gmail.com>
2022-05-25 17:11:54 +0530

    full screen

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
-import ReactPlayer from 'react-player'
+import ReactPlayer from 'react-player/youtube'
 
 export default function App() {
-  return (
-    <div>
-        <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
-    </div>
-  )
+    return (
+        <div>
+            <ReactPlayer
+                width="100%"
+                height="90%"
+                url='https://www.youtube.com/watch?v=yIe3anl_2Ks'
+            />
+        </div>
+    )
 }
