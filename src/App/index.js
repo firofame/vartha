3445bb37223aa237c894303c19b4d1e214d2ea26
Firoz Ahmed <Firoz.Ahmed@hcahealthcare.com>
2022-06-16 11:40:41 +0530

    adjust video height

--- src/App/index.js
+++ src/App/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 const channelList = [
   { name: 'Media One', channelId: 'UC-f7r46JhYv78q5pGrO6ivA' },
@@ -12,18 +12,28 @@ const channelList = [
 
 export default function App() {
   const [channelId, setChannelId] = useState(channelList[0].channelId)
+  const [buttonHeight, setButtonHeight] = useState(0)
+  
   const isLandscape = window.innerWidth > window.innerHeight
+
+  const ref = useRef(null)
+
+  useEffect(() => {
+    if(ref.current.clientHeight&&ref.current.clientHeight!==buttonHeight) {
+      setButtonHeight(ref.current.clientHeight)
+    }
+  }, [buttonHeight])
 
   return (
     <div>
       <iframe
         title='youtube'
         width="100%"
-        height={isLandscape ? '100%' : window.innerWidth * 9 / 16}
+        height={isLandscape ? window.innerHeight - buttonHeight : window.innerWidth * 9 / 16}
         src={"https://www.youtube.com/embed/live_stream?channel=" + channelId + "&autoplay=1"}
         frameBorder="0"
         allow="fullscreen; autoplay; picture-in-picture;" />
-      <div style={{ textAlign: 'center', paddingTop: 20, marginLeft: 16 }} >
+      <div ref={ref} style={{ textAlign: 'center', paddingTop: 20, marginLeft: 16 }}>
         {channelList.map((channel, index) => {
           return (
             <button
